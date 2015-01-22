35ef7dce7bb8c63c0e15b0ade3fa700ba092040d
Timeron <timeron@gmail.com>
2015-01-22 17:12:58 +0100

    Refactor

--- src/main/java/com/nexus/apps/wallet/draft/DraftDayByDay.java
+++ src/main/java/com/nexus/apps/wallet/draft/DraftDayByDay.java
@@ -1,5 +1,23 @@
 package com.nexus.apps.wallet.draft;
 
+import java.util.List;
+
+import com.nexus.dao.entity.WalletRecord;
+
 public class DraftDayByDay {
 
+	private List<WalletRecord> recordList;
+
+	public List<WalletRecord> getRecordList() {
+		return recordList;
+	}
+
+	public void setRecordList(List<WalletRecord> recordList) {
+		this.recordList = recordList;
+	}
+	
+	
+	
+	
+	
 }
