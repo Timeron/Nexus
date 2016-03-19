8d7ca358047160e59f8e298d72f41af71e85bca5
Timeron <timeron@gmail.com>
2016-03-19 22:29:18 +0100

    NEXUS-9

--- src/main/test/apps/jTask/JTaskRestServiceHelperTest.java
+++ src/main/test/apps/jTask/JTaskRestServiceHelperTest.java
@@ -4,15 +4,28 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.util.List;
 
+import org.dbunit.IDatabaseTester;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import com.nexus.apps.jTask.dto.bean.JHistoryDTO;
 import com.nexus.apps.jTask.dto.bean.JNoteDTO;
@@ -27,6 +40,7 @@ import com.timeron.NexusDatabaseLibrary.dao.JTaskDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/simple-job-launcher-context.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
 @WebAppConfiguration
 public class JTaskRestServiceHelperTest {
 
@@ -37,18 +51,40 @@ public class JTaskRestServiceHelperTest {
 	@Autowired
 	JTaskRestServiceHelper testClass;
 	
-	@Before
-	public void init(){
-		
-	}
+//	@SuppressWarnings("unused")
+//	@BeforeClass
+//	public void importDataSet() throws Exception {
+//		IDataSet dataSet = readDataSet();
+////		cleanlyInsertDataset(dataSet);
+//	}
+//	
+//	private IDataSet readDataSet() throws Exception {
+//		return new FlatXmlDataSetBuilder().build(new File("/dataset.xml"));
+//	}
+//	
+//	private void cleanlyInsertDataset(IDataSet dataSet) throws Exception {
+//		IDatabaseTester databaseTester = new JdbcDatabaseTester(
+//			"org.h2.Driver", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "");
+//		databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+//		databaseTester.setDataSet(dataSet);
+//		databaseTester.onSetup();
+//	}
+	
+//	@Test
+//	public void testRemove() throws Exception {
+//		jProjectDAO
+//	}
 	
 	@Test
+	@DatabaseSetup("/dbMock/JProject.xml")
 	public void getProjectList() {
 		List<JProjectDTO> result = testClass.getProjectList();
 		assertNotNull(result);
+		assertEquals(4, result.size());
 	}
 	
 	@Test
+	@DatabaseSetup("/dbMock/JProject.xml")
 	public void addNewProject(){
 		JProjectDTO jProjectDTO = new JProjectDTO();
 		jProjectDTO.setDescription("testDescription");
@@ -59,19 +95,26 @@ public class JTaskRestServiceHelperTest {
 		result = testClass.addNewProject(jProjectDTO, result);
 		
 		assertNotNull(result);
-//		assertTrue(result.isSuccess());
-//		assertNotNull(result.getObject());
+		assertTrue(result.isSuccess());
+		assertNotNull(result.getObject());
+		assertEquals(jProjectDTO, testClass.getProjectList().get(testClass.getProjectList().size()-1));
 		
+		jProjectDTO = new JProjectDTO();
+		jProjectDTO.setDescription("testDescription");
+		jProjectDTO.setName("testName");
+		jProjectDTO.setPrefix("testPrefix");
 	}
 	
 	@Test
 	public void addNewTask(){
+		//TODO
 		assertTrue(true);
 //		testClass.addNewTask(jTaskDTO, result, principal)
 	}
 	
 	@Test
 	public void getProjectTasksList(){
+		//TODO
 		List<JTaskDTO> tasks = testClass.getProjectTasksList(1);
 		assertNotNull(tasks);
 		assertTrue(tasks.size() > 0 ? false : true);
@@ -80,6 +123,7 @@ public class JTaskRestServiceHelperTest {
 	
 	@Test
 	public void getTaskList(){
+		//TODO
 		List<JTaskDTO> tasks = testClass.getTaskList();
 		assertNotNull(tasks);
 		assertTrue(tasks.size() > 0 ? true : false);
@@ -87,12 +131,14 @@ public class JTaskRestServiceHelperTest {
 	
 	@Test
 	public void getTask(){
+		//TODO
 		JTaskDTO task = testClass.getTask(92);
 		assertNotNull(task);
 	}
 
 	@Test
 	public void getProjectPrefix(){
+		//TODO
 		String prefix = testClass.getProjectPrefix(21);
 		assertEquals("NEXUS", prefix);
 		
@@ -100,6 +146,7 @@ public class JTaskRestServiceHelperTest {
 	
 	@Test
 	public void updateTask(){
+		//TODO
 		ServiceResult result = new ServiceResult();
 		JTaskDTO jTaskDTO = testClass.getTask(92);
 		String desc = jTaskDTO.getDescription()+"A";
@@ -114,6 +161,7 @@ public class JTaskRestServiceHelperTest {
 	
 	@Test
 	public void getTaskHistory(){
+		//TODO
 		List<JHistoryDTO> history = testClass.getTaskHistory(92);
 		System.out.println(history);
 		assertNotNull(history);
@@ -121,6 +169,7 @@ public class JTaskRestServiceHelperTest {
 	
 	@Test
 	public void getTaskNotes(){
+		//TODO
 		List<JNoteDTO> notes = testClass.getTaskNotes(1);
 		assertNotNull(notes);
 		
@@ -128,11 +177,13 @@ public class JTaskRestServiceHelperTest {
 	
 	@Test
 	public void saveNote(){
+		//TODO
 		assertTrue(true);
 	}
 	
 	@Test
 	public void getNextName(){
+		//TODO
 		JTask task = null;
 		String nextName = testClass.getNextName(jTaskDAO.getById(1), "TEST");
 		assertEquals("TEST-1", nextName);
@@ -148,12 +199,14 @@ public class JTaskRestServiceHelperTest {
 	
 	@Test
 	public void getAppVersion(){
+		//TODO
 		assertTrue(true);
 //		testClass.getAppVersion("Wallet");
 	}
 	
 	@Test
 	public void getAllUsers(){
+		//TODO
 		List<NexusPersonDTO> users = testClass.getAllUsers();
 		assertNotNull(users);
 		assertTrue(users.size() > 0 ? true : false);
@@ -161,11 +214,13 @@ public class JTaskRestServiceHelperTest {
 	
 	@Test
 	public void assignTaskToUser(){
+		//TODO
 		assertTrue(true);
 	}
 	
 	@Test
 	public void setMainTask(){
+		//TODO
 		assertTrue(true);
 	}
 }
