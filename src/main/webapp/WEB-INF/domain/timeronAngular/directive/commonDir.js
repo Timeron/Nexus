e90ad139e64362f3ac444ce1b4016001bfb594da
Timeron <timeron@gmail.com>
2016-04-09 11:28:15 +0200

    Refactor

---
+++ src/main/webapp/WEB-INF/domain/timeronAngular/directive/commonDir.js
@@ -0,0 +1,32 @@
+var dir = angular.module('CommonDir', [])
+
+.directive("markLine", function(){
+	return {
+		restrict: "AE",
+		link: function(scope, element, attrs){
+			element.bind('mouseover', function(){
+				element[0].style.backgroundColor = "#E0FFE0";
+			});
+			element.bind('mouseout', function(){
+				element[0].style.backgroundColor = "#FFF";
+			});
+			element.attr('data-dismiss', 'modal');
+			
+		}
+	};
+})
+
+.directive("removeFromView", function(){
+	return {
+		restrict: "AE",
+		controller: function($scope, $element, $attrs){
+			$attrs.$observe('hide', function(e) {
+				if(e === "false"){
+					$element[0].hidden = true;
+				}else{
+					$element[0].hidden = false;
+				}
+			});
+		}
+	};
+});
