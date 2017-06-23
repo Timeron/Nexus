d4de2e9ca691833b00bcf4d4b9eae5b3c795a50b
Timeron <timeron@gmail.com>
2017-06-23 20:47:30 +0200

    JTASK-68

--- src/main/test/apps/jTask/JTaskAllTests.java
+++ src/main/test/apps/jTask/JTaskAllTests.java
@@ -4,10 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import apps.jTask.JTaskRestServiceHelperIntegrationTest;
+
+
 @RunWith(Suite.class)
-@SuiteClasses({
-	JTaskRestServiceHelperIntegrationTest.class, 
-	JTaskRestServiceHelperTest.class})
+@SuiteClasses(
+	JTaskRestServiceHelperIntegrationTest.class)
 public class JTaskAllTests {
 
 }
